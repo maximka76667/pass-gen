3b2c9f28498eac109d4a9f07f5fb25a039a3e56e
max76667 <max766667@gmail.com>
2021-08-07 18:34:24 +0600

    добавил стилей и шрифт

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import crypto from 'crypto';
 
 function App() {
 
-  const [str, setStr] = React.useState(16);
+  const [str, setStr] = React.useState('16');
   const [result, setResult] = React.useState('');
   const [resultLength, setResultLength] = React.useState(0);
 
@@ -22,7 +22,8 @@ function App() {
   }
 
   function handleCopy(e) {
-    navigator.clipboard.writeText(result)
+    if(result) {
+      navigator.clipboard.writeText(result)
       .then(() => {
         const tmp = result;
         setResult('Скопировано!');
@@ -33,25 +34,33 @@ function App() {
       .catch(err => {
         console.log(err);
       });
+    }
   }
 
   function generatePass(str) {
     return crypto.randomBytes(str).toString('hex');
   }
 
-
   return (
     <div className="app">
-      <h1 className="heading">Generate your password!</h1>
-      <form className="form" onSubmit={handleSubmit}>
-        <input className="input" id="input" value={str} onChange={handleChange} placeholder="Nubmer from 0 to 32" />
-        <button className="submit-button" type="submit">Generate Password</button>
-      </form>
-      <div className="result-wrapper">
-        <p className="result">{result}</p>
-        <button className="copy-button" onClick={handleCopy}>Copy</button>
+      <span style={{"--i": 0}}></span>
+      <span style={{"--i": 1}}></span>
+      <span style={{"--i": 2}}></span>
+      <span style={{"--i": 3}}></span>
+      <div className="glass">
+        <h1 className="heading">Generate your password!</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <input className="input" id="input" value={str} onChange={handleChange} placeholder="Number from 0 to 32" />
+          <button className="submit-button" type="submit" disabled = { str ? false : true } >Generate Password</button>
+        </form>
       </div>
-      <p className="result-length">Length: {resultLength}</p>
+      <div className="glass">
+        <div className="result-wrapper">
+          <p className="result">{result}</p>
+          <button className="copy-button" onClick={handleCopy} disabled = { result ? false : true }>Copy</button>
+        </div>
+        <p className="result-length">Length: {resultLength}</p>
+      </div>
     </div>
   );
 }
