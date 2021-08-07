852d55e55e7ccb5502a86c9eb0f530f5cd38b71f
max76667 <max766667@gmail.com>
2021-08-07 14:58:33 +0600

    изменил алгоритм генерации кода

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import React from "react";
+import crypto from 'crypto';
 
 function App() {
 
-  const [str, setStr] = React.useState('');
+  const [str, setStr] = React.useState(16);
   const [result, setResult] = React.useState('');
+  const [resultLength, setResultLength] = React.useState(0);
 
   function handleChange(e) {
+    if(e.target.value >= 0 && e.target.value <= 32)
     setStr(e.target.value);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    setResult(generatePass(str));
+    const result = generatePass(+str);
+
+    setResult(result);
+    setResultLength(result.length);
+  }
+
+  function handleCopy(e) {
+    navigator.clipboard.writeText(result)
+      .then(() => {
+        const tmp = result;
+        setResult('Скопировано!');
+        setTimeout(() => {
+          setResult(tmp);
+        }, 500)
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   function generatePass(str) {
-    let array = str.split('');
-
-    for (let i = array.length-1; i > 0; i--) {
-      if(i % 2 === 0) {
-        let t = array[i-1]; array[i-1] = array[i]; array[i] = t
-      }
-    }
-    return array.map((l) => l.charCodeAt(0).toString(16)).filter((l, i) => (i + 1) % 2 === 0).join('');
+    return crypto.randomBytes(str).toString('hex');
   }
 
 
@@ -31,12 +44,14 @@ function App() {
     <div className="app">
       <h1 className="heading">Generate your password!</h1>
       <form className="form" onSubmit={handleSubmit}>
-        <input className="input" id="input" value={str} onChange={handleChange} placeholder="Input Stroke" />
-        <p className="str">Length: {str.length}</p>
+        <input className="input" id="input" value={str} onChange={handleChange} placeholder="Nubmer from 0 to 32" />
         <button className="submit-button" type="submit">Generate Password</button>
       </form>
-      <p className="result">{result}</p>
-      <p className="result">Length: {result.length}</p>
+      <div className="result-wrapper">
+        <p className="result">{result}</p>
+        <button className="copy-button" onClick={handleCopy}>Copy</button>
+      </div>
+      <p className="result-length">Length: {resultLength}</p>
     </div>
   );
 }
